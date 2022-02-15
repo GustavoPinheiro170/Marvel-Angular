4205be71b4058a5bef6e5c58173492009edf1c51
GustavoPinheiro170 <gustavocampos170@hotmail.com>
2022-02-15 00:58:35 -0300

    media screen loading

--- src/assets/helpers/components/loading/loading.component.ts
+++ src/assets/helpers/components/loading/loading.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loading',
   template: '<img src="../../../../assets/loading.gif" />',
+  styles: ['@media screen and (max-width:800px){ img { width:250px }}']
+  ,
 })
 export class LoadingComponent{
   constructor() { }
